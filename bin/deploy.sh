dbb42dbab6e7de2052a9df561f2cbf0f6413c9ea
miya0001 <miya@wpist.me>
2016-04-11 18:39:29 +0900

    add deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	exit
+fi
+
+echo ".travis.yml
+bin
+gulpfile.js
+node_modules
+package.json
+tests
+wapuu.csv" > .gitignore
+
+git init
+git config user.name "Travis CI"
+git config user.email "miya+github.com@wpist.me"
+git add .
+git commit --quiet -m "Deploy from travis"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
