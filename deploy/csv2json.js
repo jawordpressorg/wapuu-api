6df601227206bdbbb40c60311dc98a8f2f00c082
Toshihiro Kanai <i@miruc.co>
2019-04-27 18:44:51 +0900

    fix: Broken path to JSON

--- deploy/csv2json.js
+++ deploy/csv2json.js
@@ -1,33 +1,37 @@
-var fs = require('fs');
-var csv = require("comma-separated-values");
+const fs = require('fs')
+const csv = require('comma-separated-values')
 
 fs.readFile('../wapuu.csv', 'utf8', function (err, text) {
-  var result = new csv(text, { header: true }).parse();
-  var data = [];
-  for (var i=0; i<result.length; i++) {
-    var line = result[i];
-    var row = {};
-    row.name = line.id;
-    row.wapuu = {};
-    row.wapuu.name = line['wapuu.name'];
-    row.wapuu.url = line['wapuu.url'];
-    row.wapuu.repository = line['wapuu.repository'];
-    row.wapuu.src = line['wapuu.src'];
-    row.wapuu.mime_type = line['wapuu.mime_type'];
-    row.author = {};
-    row.author.name = line['author.name'];
+  const result = new csv(text, {header: true}).parse()
+  const data = []
+  for (let i = 0; i < result.length; i++) {
+    const line = result[i]
+    const row = {}
+    row.name = line.id
+    row.wapuu = {}
+    row.wapuu.name = line['wapuu.name']
+    row.wapuu.url = line['wapuu.url']
+    row.wapuu.repository = line['wapuu.repository']
+    row.wapuu.src = line['wapuu.src']
+    row.wapuu.mime_type = line['wapuu.mime_type']
+    row.author = {}
+    row.author.name = line['author.name']
     if (line['author.url']) {
-      row.author.url = line['author.url'];
+      row.author.url = line['author.url']
     } else {
-      row.author.url = "";
+      row.author.url = ''
     }
     if (line['description']) {
-      row.description = line['description'];
+      row.description = line['description']
     } else {
-      row.description = "";
+      row.description = ''
     }
-    data.push(row);
+    data.push(row)
   }
 
-  fs.writeFile('v1/wapuu.json', JSON.stringify(data));
-});
+  fs.writeFile('v1/wapuu.json', JSON.stringify(data), (err) => {
+    if (err) {
+      throw new err
+    }
+  })
+})
